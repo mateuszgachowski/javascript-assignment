f568488202ef3f0857533ae4043dc07c8f203be1
Mateusz Gachowski <gachu@MacBook-Pro-Mateusz.local>
2016-09-22 00:49:29 +0200

    Assignment 7 solved

--- ex07.js
+++ ex07.js
@@ -1,12 +1,14 @@
 /*
-Given an Array of strings, use Array#reduce to create an object that contains the number of times each string occured in the array. Return the object directly (no need to console.log).
+Given an Array of strings, use Array#reduce to create an object that contains
+the number of times each string occured in the array. Return the object
+directly (no need to console.log).
 
 Example
 
     var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
-    
+
     console.log(countWords(inputWords))
-    
+
     // =>
     // {
     //   Apple: 2,
@@ -15,3 +17,14 @@ Example
     // }
 
 */
+
+var countWords = function (inputWords) {
+    return inputWords.reduce(function (wordList, word) {
+        // Increment current value or set to one for first occurence
+        wordList[word] = (wordList[word] + 1) || 1;
+        return wordList;
+    }, {});
+}
+var inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian'];
+
+console.log(countWords(inputWords));
