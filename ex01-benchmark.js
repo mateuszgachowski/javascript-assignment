ec3ad6c984e7d1162a15c5a6758adb511279cfa1
Mateusz Gachowski <gachu@MacBook-Pro-Mateusz.local>
2016-09-21 11:01:12 +0200

    First assignment + benchmark for bit shift operation

---
+++ ex01-benchmark.js
@@ -0,0 +1,31 @@
+var _ = require('lodash');
+var Benchmark = require('benchmark');
+
+// Function defs
+var power = _.partial(function (exponent) {
+    return 2 << (exponent - 1);
+});
+
+var powerPow = _.partial(function (exponent) {
+    return Math.pow(2, exponent);
+});
+
+
+// Test suite
+var suite = new Benchmark.Suite;
+suite.add('Shift bit', function() {
+    power(30)
+})
+suite.add('Math pow', function() {
+    powerPow(30)
+})
+.on('cycle', function(event) {
+    console.log(String(event.target));
+})
+.on('complete', function() {
+    var fastest = this.filter('fastest');
+    var slowest = this.filter('slowest');
+    var percentage = slowest.map('hz') / fastest.map('hz');
+    console.log('Fastest is ' + fastest.map('name') + ': ' + Math.round(percentage * 100) + '%');
+})
+.run({ 'async': true });
--- ex01.js
+++ ex01.js
@@ -1,20 +1,49 @@
 // use _.partial function to implement functions:
-
 var _ = require('lodash');
 
-var addFiveAndSixAnd = _.partial(/* your code */);
-var power = _.partial(/* your code */);
-var powerTwo = _.partial(/* your code */);
-var powerThree = _.partial(/* your code */);
+var addFiveAndSixAnd = _.partial(function () {
+    // Function can take more than one param and sum them all together
+    var args = Array.prototype.slice.call(arguments);
+
+    args.push(5, 6);
+
+    return _.sum(args);
+});
+
+var power = _.partial(function (exponent) {
+    // Exponent of two is much faster (from ~70% to even ~80%) for numbers in range 0 to 30
+    // From 30 it gives negative numbers due to int-number limitation
+    // See ex01-benchmark.js file for more info
+    // Depends on the needs really :)
+    if (exponent < 0 || exponent > 30) {
+        throw Error('This functions gives correct values only on 0-30 range of exponent');
+    }
+    return 2 << (exponent - 1);
+
+    // Normal pow using Math lib, slower but more convinient outside 0-30 range
+    // If using Math.pow we could use lodash _.bind for partially passed args
+    // as in functions below
+    // return Math.pow(2, exponent);
+});
+
+var powerTwo = _.partial(_.bind(Math.pow, Math, _, 2));
+
+var powerThree = _.partial(_.bind(Math.pow, Math, _, 3));
 
 
 console.log(addFiveAndSixAnd(4)); //15 (adds 5 + 6 + 4 = 15)
+// console.log(addFiveAndSixAnd(4, 6, 7)); //28 (adds 5 + 6 + 4 + 6 + 7 = 28)
 
 //Evaluates Math.power(2,3)
 console.log(power(3)); //8
+// console.log(power(4)); //16
+// console.log(power(10)); //1024
+// console.log(power(16)); //65536
+// console.log(power(31)); // throws error, outside the range
 
-//Evaluates Math.power(3,2)
+
+// //Evaluates Math.power(3,2)
 console.log(powerTwo(3)); //9
-
-//Evaluates Math.power(3,3)
+//
+// //Evaluates Math.power(3,3)
 console.log(powerThree(3)); // 27
