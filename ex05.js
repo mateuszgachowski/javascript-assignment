257e00411e9b8fd4c75873cf9142e961f1e174af
Mateusz Gachowski <gachu@MacBook-Pro-Mateusz.local>
2016-09-21 23:32:47 +0200

    Assignment 5 solved

--- ex05.js
+++ ex05.js
@@ -12,11 +12,35 @@ The function should return an array containing the messages themselves, without 
     {
       message: 'Esse id amet quis eu esse aute officia ipsum.' // random
     }
-  
+
   Hint - try chaining some Array methods!
 
 */
 
-function getShortMessages(){
-  /* your code */
+var sampleData = [
+    {
+        message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida velit velit, ac ultrices tellus cursus quis. Donec fringilla mattis purus, at pellentesque nunc ultricies id. Pellentesque bibendum ante id ipsum vestibulum, auctor sollicitudin lectus elementum. Donec metus tortor, auctor eu dapibus sed, viverra vitae ante.'
+    },
+    {
+        message: 'Fusce convallis nibh ex, ac gravida lectus ultricies elementum. Sed tristique id magna ut sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        message: 'Donec leo tellus, gravida ut ex quis, scelerisque mattis erat. Aliquam erat volutpat. Maecenas ultrices lorem non augue dignissim scelerisque.'
+    },
+    {
+        message: 'Donec leo tellus, gravida ut ex quis.'
+    }
+]
+
+function getShortMessages(messages) {
+
+    return messages
+        .map(function (element) {
+            return element.message;
+        })
+        .filter(function(message) {
+            return message.length < 50
+        });
 }
+
+console.log(getShortMessages(sampleData));
