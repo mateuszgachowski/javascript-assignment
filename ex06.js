65037074b1feadd9859994dff7e7b44619457a14
Mateusz Gachowski <gachu@MacBook-Pro-Mateusz.local>
2016-09-22 00:41:37 +0200

    Assignment 6 solved

--- ex06.js
+++ ex06.js
@@ -1,5 +1,6 @@
 /*
-Return a function that takes a list of valid users, and returns a function that returns true if all of the supplied users exist in the original list of users.
+Return a function that takes a list of valid users, and returns a function that
+returns true if all of the supplied users exist in the original list of users.
 
 You only need to check that the ids match.
 
@@ -10,16 +11,16 @@ You only need to check that the ids match.
       { id: 2 },
       { id: 3 }
     ]
-    
+
     // `checkUsersValid` is the function you'll define
     var testAllValid = checkUsersValid(goodUsers)
-    
+
     testAllValid([
       { id: 2 },
       { id: 1 }
     ])
     // => true
-    
+
     testAllValid([
       { id: 2 },
       { id: 4 },
@@ -33,3 +34,39 @@ You only need to check that the ids match.
 
 Tip: you can use array#some and Array#every or _.includes
 */
+
+var _ = require('lodash');
+
+var goodUsers = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 }
+];
+
+var checkUsersValid = function (goodUsers) {
+    return function (testedUsers) {
+        return testedUsers.every(function (testUser) {
+            // Oneliner but compicates performance with `map`
+            // return _.includes(_.map(goodUsers, 'id'), testUser.id);
+            return goodUsers.some(function (goodUser) {
+                return testUser.id === goodUser.id;
+            });
+        });
+    };
+};
+
+var testAllValid = checkUsersValid(goodUsers);
+
+var a = testAllValid([
+  { id: 2 },
+  { id: 1 }
+]);
+
+var b = testAllValid([
+  { id: 2 },
+  { id: 4 },
+  { id: 1 }
+]);
+
+
+console.log(a, b);
