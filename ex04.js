c3c79fb377bf5c66cb49b4526aca6c91c7c10d8b
Mateusz Gachowski <gachu@MacBook-Pro-Mateusz.local>
2016-09-21 23:30:14 +0200

    Assignment 4 solved

--- ex04.js
+++ ex04.js
@@ -3,10 +3,18 @@
 // * Do not use any for/while loops or Array.prototype.forEach.
 // * Do not create any unnecessary functions e.g. helpers.
 
+// function doubleAll(numbers) {
+//   var result = []
+//   for (var i = 0; i < numbers.length; i++) {
+//     result.push(numbers[i] * 2)
+//   }
+//   return result
+// }
+
 function doubleAll(numbers) {
-  var result = []
-  for (var i = 0; i < numbers.length; i++) {
-    result.push(numbers[i] * 2)
-  }
-  return result
+    return numbers.map(function (number) {
+        return number * 2;
+    });
 }
+
+console.log(doubleAll([1, 2, 4, 5, 5]));
